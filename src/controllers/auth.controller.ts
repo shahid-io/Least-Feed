2bca4d267e48de6d128b9954fb0230cde5a49ad5
shahid <razashahid532@gmail.com>
2024-03-22 10:15:38 +0530

    feat: hashed password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,7 +8,9 @@ import { ResultSetHeader } from 'mysql2';
 import { Feed } from '../interface/feed.interface';
 import { Auth } from '../interface/auth.interface';
 import { AUTH_MESSAGE } from '../enum/auth.message.enum';
-
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv'
+dotenv.config()
 export const getUsers = async (req: Request, res: Response) => {
     console.info(`[${new Date().toLocaleString()}] Incoming ${req.method}${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
     try {
@@ -46,8 +48,16 @@ export const createUser = async (req: Request, res: Response): Promise<Response<
     let user: Auth = { ...req.body };
     try {
         const pool = await connection();
+        let saltRounds = 10; 
+        const saltRoundsEnv = process.env.SALT_ROUND;
+        if (saltRoundsEnv && !isNaN(parseInt(saltRoundsEnv))) {
+            saltRounds = parseInt(saltRoundsEnv);
+        }
+        const hash = await bcrypt.hash(user.password, saltRounds)
+        user.password = hash;
         const result: any = await pool.query(AUTH_QUERY.CREATE_USER, Object.values(user));
-        user = { id: (result[0] as ResultSetHeader).insertId, ...req.body };
+        user = { id: (result[0]).insertId, ...user };
+        delete (user as { password?: string }).password;
         return res.status(Code.CREATED)
             .send(new HttpResponse(Code.CREATED, Status.CREATED, AUTH_MESSAGE.CREATED_SUCCESS, user));
     } catch (error: unknown) {
@@ -64,9 +74,9 @@ export const updateUser = async (req: Request, res: Response): Promise<Response<
         const pool = await connection();
         const result: any = await pool.query(AUTH_QUERY.SELECT_USER, [req.params.userId]);
         if (((result[0]) as Array<any>).length > 0) {
-            const result: any = await pool.query(AUTH_QUERY.UPDATE_USER, [...Object.values(user), req.params.feedId]);
+            const result: any = await pool.query(AUTH_QUERY.UPDATE_USER, [...Object.values(user), req.params.userId]);
             return res.status(Code.OK)
-                .send(new HttpResponse(Code.OK, Status.OK, AUTH_MESSAGE.UPDATED_SUCCESS, { ...user, id: req.params.feedId }));
+                .send(new HttpResponse(Code.OK, Status.OK, AUTH_MESSAGE.UPDATED_SUCCESS, { ...user, id: req.params.userId }));
         } else {
             return res.status(Code.NOT_FOUND)
                 .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, AUTH_MESSAGE.NOT_FOUND));
